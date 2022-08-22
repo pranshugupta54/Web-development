394abdf8d4a066a0c24249c738eb049a640755e6
Pranshu <pranshgupta54@gmail.com>
2022-08-22 18:28:24 +0530

    update

--- html/learning/using bootstrap/new.js
+++ html/learning/using bootstrap/new.js
@@ -1,4 +1,18 @@
-function addFunction(TaskText,ListId){
+// function addFunction(TaskText,ListId){
+//     if(TaskText == ""){
+//         alert("No input bro")
+//         return;
+//     }
+//     var addText = TaskText;
+//     let newDiv = document.createElement("li");
+//     newDiv.classList.add('list-group-item')
+//     newDiv.innerHTML = `${addText}`;
+//     ListId.append(newDiv);
+// }
+
+function addFunction(InputBoxId,ListId){
+    //console.log(InputBoxId.value);
+    TaskText = InputBoxId.value;
     if(TaskText == ""){
         alert("No input bro")
         return;
@@ -8,4 +22,5 @@ function addFunction(TaskText,ListId){
     newDiv.classList.add('list-group-item')
     newDiv.innerHTML = `${addText}`;
     ListId.append(newDiv);
+    InputBoxId.value = ``;    
 }
